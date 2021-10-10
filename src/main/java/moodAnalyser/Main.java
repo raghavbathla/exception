8dcaebfea118b753216400fad3f795ce4da64e7d
Raghav <bathla.raghav5@gmail.com>
2021-10-11 01:24:14 +0530

    [Raghav] Empty string added

--- src/main/java/moodAnalyser/Main.java
+++ src/main/java/moodAnalyser/Main.java
@@ -3,11 +3,13 @@ package moodAnalyser;
 public class Main {
     public static void main(String[] args) throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-
+        MoodAnalyser moodAnalyser1 = new MoodAnalyser("");
+        MoodAnalyser moodAnalyser2 = new MoodAnalyser("I am in sad Mood");
+        MoodAnalyser moodAnalyser3 = new MoodAnalyser("I am in happy Mood");
         System.out.println(moodAnalyser.analyseMood());
-//        System.out.println(moodAnalyser1.analyseMood());
-//        System.out.println(moodAnalyser2.analyseMood());
-//        System.out.println(moodAnalyser3.analyseMood());
+        System.out.println(moodAnalyser1.analyseMood());
+        System.out.println(moodAnalyser2.analyseMood());
+        System.out.println(moodAnalyser3.analyseMood());
 
     }
 }
