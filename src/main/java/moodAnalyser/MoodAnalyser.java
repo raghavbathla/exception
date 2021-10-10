c05ed58047528d59c5460d25acd495439e81ba17
Raghav <bathla.raghav5@gmail.com>
2021-10-10 20:43:09 +0530

    [Raghav] Return sad mood

--- src/main/java/moodAnalyser/MoodAnalyser.java
+++ src/main/java/moodAnalyser/MoodAnalyser.java
@@ -1,8 +1,10 @@
 package moodAnalyser;
 
 public class MoodAnalyser {
-    public String analyseMood(){
-
+    public String analyseMood(String mood){
+if(mood.equals("I am in sad Mood")){
+    return "SAD";
+}
 
     }
 }
